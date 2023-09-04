29f09dc02cf26ae9f1f3fa5aa1f71458e38f5332
Juan M Panozzo Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-09-04 10:20:57 -0300

    modificados:     package-lock.json modificados:     package.json nuevos archivos: src/app/signup/loading.tsx nuevos archivos: src/app/signup/page.tsx nuevos archivos: src/components/ui/select.tsx

---
+++ src/app/signup/loading.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+    return <>Loading...</>;
+};
+
+export default Register;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import { Container, Section, Box } from "@radix-ui/themes";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+    name: z
+        .string()
+        .min(3, { message: "El nombre debe tener al menos 3 caracteres" }),
+    lastname: z
+        .string()
+        .min(3, { message: "El apellido debe tener al menos 3 caracteres" }),
+    area: z
+        .string()
+        .min(3, { message: "El area debe tener al menos 3 caracteres" }),
+    email: z.string().email({
+        message: "Por favor, ingrese un correo válido",
+    }),
+    password: z
+        .string()
+        .min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
+});
+
+const Register = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            lastname: "",
+            area: "",
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    };
+
+    return (
+        <Section className="min-h-screen grid place-items-center">
+            <Container className="rounded-xl border p-6 shadow-md">
+                <h1 className="text-2xl font-bold">Registrarse</h1>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nombre</FormLabel>
+                                    <Input placeholder="Nombre" {...field} />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="lastname"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Apellido</FormLabel>
+                                    <Input placeholder="Apellido" {...field} />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="area"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Area</FormLabel>
+                                    <Select>
+                                        <SelectTrigger className="w-full">
+                                            <SelectValue placeholder="Area" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectItem value="1">
+                                                    Area 1
+                                                </SelectItem>
+                                                <SelectItem value="2">
+                                                    Area 2
+                                                </SelectItem>
+                                                <SelectItem value="3">
+                                                    Area 3
+                                                </SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Correo electrónico</FormLabel>
+                                    <Input
+                                        placeholder="ejemplo@correo.com"
+                                        {...field}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Contraseña</FormLabel>
+                                    <Input
+                                        type="password"
+                                        placeholder="********"
+                                        {...field}
+                                    />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Registrarse</Button>
+                    </form>
+                </Form>
+            </Container>
+        </Section>
+    );
+};
+
+export default Register;
