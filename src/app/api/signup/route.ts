897132b1245233d010f7dc87c0c8e8670e8a096c
Juan M Panozzo Z <juanmanuel.panozzozenere@alumnos.uai.edu.ar>
2023-09-05 12:54:20 -0300

    modificados:     src/app/api/signup/route.ts

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,44 +1,55 @@
-import prisma from "@/lib/prima";
+import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const { name, lastname, email, password } = await req.json();
+    try {
+        const { name, lastname, email, password } = await req.json();
 
-    const user = await prisma.user.findUnique({
-        where: {
-            email : email.toLowerCase() as string,
-        },
-    });
-
-    if (user) {
-        return {
-            status: 401,
-            body: {
-                error: "User already exists",
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email.toLowerCase(),
             },
-        };
-    }
+        });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+        if (existingUser) {
+            return NextResponse.json({
+                status: 401,
+                body: {
+                    error: "User already exists",
+                },
+            });
+        }
 
-    const newUser = await prisma.user.create({
-        data: {
-            name,
-            lastname,
-            email: email.toLowerCase() as string,
-            password: hashedPassword as string,
-        },
-    });
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (newUser) {
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                lastname,
+                email: email.toLowerCase(),
+                password: hashedPassword,
+            },
+        });
+
+        if (newUser) {
+            return NextResponse.json({
+                status: 200,
+                body: {
+                    message: "User created",
+                },
+            });
+        }
+
+        console.log(newUser);
+    } catch (error) {
+        console.error(error);
+
         return NextResponse.json({
-            status: 200,
+            status: 500,
             body: {
-                message: "User created",
+                error: "Internal server error",
             },
         });
     }
-
-    console.log(newUser);
 }
